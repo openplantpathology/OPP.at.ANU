90f4bfb3b8356ad3322c657dbf6874d48c8e599e
Adam H Sparks <adamhsparks@gmail.com>
2018-06-09 17:29:40 +1000

    Update to version 0.0.1 for DOI and add package documentation

---
+++ R/OPP_at_IEW218.R
@@ -0,0 +1,8 @@
+#' Supplementary materials for EM Del Ponte and AH Sparks' presentation at the
+#' International Epidemiology Workshop 12 in Norway, 2018.
+#'
+#' @name OPP.at.IEW2018
+#' @author Adam H Sparks and Emerson M Del Ponte
+#' @docType package
+#'
+NULL
